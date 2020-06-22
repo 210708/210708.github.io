41e6b5141cb476056b2db7c15f7840b31fe043ff
210708 <67256073+210708@users.noreply.github.com>
2020-06-22 19:05:44 +0900

    Update Redirect.js

--- Redirect.js
+++ Redirect.js
@@ -1,9 +1,34 @@
-var MAX = 8;
+var MAX = 9;
 var Rand = Math.floor(Math.random() * MAX);
 
 alert(Rand);
 
 if (Rand == 0) {
-  alert('Rand is 0');
+  window.location.href = 'Battle01-OST.mp3';
 }
-//window.location.href = '/';
+else if (Rand == 1) {
+  window.location.href = 'Battle01-OST.mp3';
+}
+else if (Rand == 2) {
+  window.location.href = 'Battle01-OST.mp3';
+}
+else if (Rand == 3) {
+  window.location.href = 'Battle01-OST.mp3';
+}
+else if (Rand == 4) {
+  window.location.href = 'Battle01-OST.mp3';
+}
+else if (Rand == 5) {
+  window.location.href = 'Battle01-OST.mp3';
+}
+else if (Rand == 6) {
+  window.location.href = 'Battle01-OST.mp3';
+}
+else if (Rand == 7) {
+  window.location.href = 'Battle01-OST.mp3';
+}
+else if (Rand == 8) {
+  window.location.href = 'Battle01-OST.mp3';
+}
+else {
+  alert('Something is wrong!');
